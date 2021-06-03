d7c7afb51ce32aec7ade3711301d4c876534de23
Anton Kiyan <kiyananton@gmail.com>
2021-06-03 19:53:11 +0300

    ДОбавляет правки

--- build/js/script.js
+++ build/js/script.js
@@ -226,13 +226,6 @@
 
 (function () {
   $('.mobile-control-mobile__item-top').click(function () {
-    var $openedItem = $('.mobile-control-mobile__item-top.opened');
-
-    if ($openedItem[0] !== $(this)[0]) {
-      $openedItem.removeClass('opened');
-      $openedItem.next().hide();
-    }
-
     $(this).toggleClass('opened');
     $(this).next().slideToggle();
   });
--- source/js/modules/mobile-control.js
+++ source/js/modules/mobile-control.js
@@ -1,11 +1,5 @@
 (function() {
   $('.mobile-control-mobile__item-top').click(function() {
-    const $openedItem = $('.mobile-control-mobile__item-top.opened');
-    if ($openedItem[0] !== $(this)[0]) {
-      $openedItem.removeClass('opened');
-      $openedItem.next().hide();
-    }
-
     $(this).toggleClass('opened');
     $(this).next().slideToggle();
   });
